d8bd36de8d20408f2e9f1239300bb2925bcd003c
Skaanka <gripon.gregoire@sfr.fr>
2015-12-10 18:22:14 +0100

    ajout des pages reseaux pro / soc / divertissement dans la branche greg

---
+++ pages/pageDivertissement.php
@@ -0,0 +1,91 @@
+<?php
+// detruit la session inscription si elle existe
+if ( isset ($_SESSION["inscription"])) {
+    unset($_SESSION["inscription"]);
+}
+
+// appel du fichier connexion bdd
+require_once('php/connexion.php');
+
+//print_r($params);
+global $db, $params;
+
+//affichage des membres
+$affichageMembre = $db->prepare('SELECT * FROM membres WHERE id = ? ');
+$affichageMembre->execute(array($_GET['query']));
+$value = $affichageMembre->fetch();
+?>
+
+
+<div>
+    <h2> [PRENOM] utilise ces plateformes de jeux online. </h2> <!-- TODO inserer en PHP le nom d'utilisateur -->
+</div>
+
+<div class="row col-md-6 col-md-offset-3" id="rowName">
+    <div>
+        <img class="icons col-sm-2" src="../img/icons/PSN_logo.png">
+        <label for="psn" class="col-sm-2">PSN</label>
+        <div class="panel panel-default">
+          <div class="panel-body" id="psn">
+            adresse compte psn
+          </div>
+        </div>
+    </div>
+    
+    <div>
+        <img class="icons col-sm-2" src="../img/icons/Xbox_logo.png">
+        <label for="xbox" class="col-sm-2">XboxLive</label>
+        <div class="panel panel-default">
+          <div class="panel-body" id="xbox">
+            adresse compte xboxlive
+          </div>
+        </div>
+    </div>
+</div>
+
+<div class="row col-md-6 col-md-offset-3" id="rowName">
+    <div>
+        <img class="icons col-sm-2" src="../img/icons/Steam_logo.png">
+        <label for="steam" class="col-sm-2">Steam</label>
+        <div class="panel panel-default">
+          <div class="panel-body" id="steam">
+            adresse compte steam
+          </div>
+        </div>
+    </div>
+    
+    <div>
+        <img class="icons col-sm-2" src="../img/icons/Xbox_logo.png">
+        <label for="battlenet" class="col-sm-2">Battle.net</label>
+        <div class="panel panel-default">
+          <div class="panel-body" id="battlenet">
+            adresse compte Battle.net
+          </div>
+        </div>
+    </div>
+</div>
+
+<div class="row col-md-6 col-md-offset-3" id="rowName">
+    <div>
+        <img class="icons col-sm-2" src="../img/icons/PSN_logo.png">
+        <label for="origin" class="col-sm-2">Origin</label>
+        <div class="panel panel-default">
+          <div class="panel-body" id="origin">
+            adresse compte Origin
+          </div>
+        </div>
+    </div>
+    
+    <div>
+        <img class="icons col-sm-2" src="../img/icons/Xbox_logo.png">
+        <label for="nintendo" class="col-sm-2">Nintendo</label>
+        <div class="panel panel-default">
+          <div class="panel-body" id="nintendo">
+            adresse compte Nintendo
+          </div>
+        </div>
+    </div>
+</div>
+
+
+
---
+++ pages/pageReseauxPro.php
@@ -0,0 +1,76 @@
+<?php
+// detruit la session inscription si elle existe
+if ( isset ($_SESSION["inscription"])) {
+    unset($_SESSION["inscription"]);
+}
+
+// appel du fichier connexion bdd
+require_once('php/connexion.php');
+
+//print_r($params);
+global $db, $params;
+
+//affichage des membres
+$affichageMembre = $db->prepare('SELECT * FROM membres WHERE id = ? ');
+$affichageMembre->execute(array($_GET['query']));
+$value = $affichageMembre->fetch();
+?>
+
+<h2> [[PRENOM]] est présent(e) sur ces réseaux professionnel. </h2> <!-- TODO inserer en PHP le nom d'utilisateur -->
+
+
+<div class="row col-md-6 col-md-offset-3" id="rowName">
+    <div>
+        <img class="icons col-sm-2" src="../img/icons/linkedin_logo.png">
+        <label for="linkedin" class="col-sm-2">LinkedIn</label>
+        <div class="panel panel-default">
+          <div class="panel-body" id="linkedin">
+            adresse compte LinkedIn
+          </div>
+        </div>
+    </div>
+    
+    <div>
+        <img class="icons col-sm-2" src="../img/icons/viadeo_logo.png">
+        <label for="viadeo" class="col-sm-2">Viadeo</label>
+        <div class="panel panel-default">
+          <div class="panel-body" id="viadeo">
+            adresse compte Viadeo
+          </div>
+        </div>
+    </div>
+</div>
+
+<div class="row col-md-6 col-md-offset-3" id="rowName">
+    <div>
+        <img class="icons col-sm-2" src="../img/icons/xing_logo.png">
+        <label for="xing" class="col-sm-2">Xing</label>
+        <div class="panel panel-default">
+          <div class="panel-body" id="xing">
+            adresse compte Xing
+          </div>
+        </div>
+    </div>
+    
+    <div>
+        <img class="icons col-sm-2" src="../img/icons/muxi_logo.jpg">
+        <label for="muxy" class="col-sm-2">Muxy</label>
+        <div class="panel panel-default">
+          <div class="panel-body" id="muxy">
+            adresse compte Muxy
+          </div>
+        </div>
+    </div>
+</div>
+
+<div class="row col-md-6 col-md-offset-3" id="rowName">
+    <div>
+        <img class="icons col-sm-2" src="../img/icons/github_logo.png">
+        <label for="github" class="col-sm-2">GitHub</label>
+        <div class="panel panel-default">
+          <div class="panel-body" id="github">
+            adresse compte Github
+          </div>
+        </div>
+    </div>    
+</div>
